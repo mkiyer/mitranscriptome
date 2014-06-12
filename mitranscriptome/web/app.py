adbc3ed85d602ca9853d6b0af0e2d9be290ec3c2
yashar.niknafs <yashar.niknafs@gmail.com>
2014-06-12 00:37:55 -0400

    :(

--- mitranscriptome/web/app.py
+++ mitranscriptome/web/app.py
@@ -297,7 +297,7 @@ def modal():
         meta['func_cat'] = 'Cancer Association'
         meta['type_show'] = 'hide'
     meta['avg_frac'] = float(format(float(meta['avg_frac']), '.4f'))
-    meta['ssea_type_img'] = SERVER_URL + '/get_ssea?transcript_id=%s&subdir=%s&plot_type=eplot' % (t_id, ssea_type)
+    meta['ssea_type_img'] = SERVER_URL1 + '/get_ssea?transcript_id=%s&subdir=%s&plot_type=eplot' % (t_id, ssea_type)
     
     meta['ssea_type_expr_img'] = SERVER_URL1 + '/get_ssea?transcript_id=%s&subdir=%s&plot_type=fpkm' % (t_id, ssea_type)        
     meta['ssea_can_img'] = SERVER_URL1 + '/get_ssea?transcript_id=%s&subdir=%s&plot_type=eplot' % (t_id, ssea_can)
