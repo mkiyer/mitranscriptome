12b5e82754db9c47c0d427ad81f509d329eaaa7a
yniknafs-mctp <yniknafs@med.umich.edu>
2015-01-21 18:24:51 -0500

    server pull fix

--- mitranscriptome/server_pull_reset.py
+++ mitranscriptome/server_pull_reset.py
@@ -6,13 +6,11 @@ Created on Dec 22, 2014
 import sys
 import logging
 import subprocess
-import fileinput
+import os
 
 
-def text_edit(search, replace, app_file):    
-    for line in fileinput.input(app_file, inplace=True):
-        print line.replace(search, replace).replace('\n', '')
-
+def replacer(line, searcher, replacer):    
+    return line.replace(searcher, replacer)
     
 def main():
     logging.basicConfig(level=logging.DEBUG,
@@ -29,19 +27,29 @@ def main():
     subprocess.call(SERVER_RESET, shell=True, cwd='/root/mitranscriptome')
     
       
-    app_file = '/root/mitranscriptome/web/app.py'
+    APP_FILE = '/root/mitranscriptome/web/app.py'
     
-    search = 'YNIKNAFS = True'
-    replace = 'YNIKNAFS = False' 
-    text_edit(search, replace, app_file)
+
+
     
-    search = 'MKIYER = True'
-    replace = 'MKIYER = False' 
-    text_edit(search, replace, app_file)
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                      format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     
-    search = 'SERVER = False'
-    replace = 'SERVER = True' 
-    text_edit(search, replace, app_file)
+      
+    APP_FILE = '/root/mitranscriptome/web'
+    TMP_FILE = 'server_bool_reset.tmp'    
+
+    with open(TMP_FILE, 'w') as f:
+        for line in open(APP_FILE):
+            line = line.replace('\n', '')
+            line = replacer(line, 'YNIKNAFS = True','YNIKNAFS = False')
+            line = replacer(line, 'MKIYER = True','MKIYER = False')
+            line = replacer(line, 'SERVER = False','SERVER = True')
+            print >>f, line
+
+    os.rename(TMP_FILE, APP_FILE)
+    
     
     
     return 0
