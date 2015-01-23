05c3d498b780d8775a704de0a9b3a2cfee5fc131
mkiyer <mkiyer@umich.edu>
2015-01-23 00:07:20 -0500

    more config stuff

--- mitranscriptome/web/wsgi.py
+++ mitranscriptome/web/wsgi.py
@@ -1,6 +1,17 @@
-# prepend website python packages to python path
+import os
 import sys
+
+# prepend website python packages to python path
 APP_ROOT = '/var/www/html/mitranscriptome/mitranscriptome'
 sys.path.insert(0, APP_ROOT)
-# load application module
-from web.app import app as application
+
+def application(environ, start_response):
+    # pass WSGI environment variables to os.environ
+    vars = ['MITRANSCRIPTOME_CONFIG']
+    for var in vars:
+        os.environ[var] = environ.get(var, 'config.Config')
+
+    # load application module
+    from web.app import app as _application
+
+    return _application(environ, start_response)
